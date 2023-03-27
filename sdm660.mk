0ef502aa81fedeface1cf3bbf943c6faa064a2a7
tebekdzn <tebekdzn@gmail.com>
2023-03-27 22:02:24 +0700

    lavender: add media minijail blobs

--- sdm660.mk
+++ sdm660.mk
@@ -278,6 +278,11 @@ PRODUCT_PACKAGES += \
     #vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.1.vendor \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
