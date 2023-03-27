ebce58a9e2fa418782d3c0ff07bd73c09524a669
tebekdzn <tebekdzn@gmail.com>
2023-03-27 18:32:52 +0700

    lavender: update vendor.display.config to 2.0

--- sdm660.mk
+++ sdm660.mk
@@ -169,7 +169,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@2.0
 
 # Doze
 PRODUCT_PACKAGES += \
